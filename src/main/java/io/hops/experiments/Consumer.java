4539000e0c102ee6a9ffc22590466187aa763a00
Salman Niazi <smkniazi@kth.se>
2018-02-20 14:18:43 +0100

    Auto generated commit msg.

--- src/main/java/io/hops/experiments/Consumer.java
+++ src/main/java/io/hops/experiments/Consumer.java
@@ -69,7 +69,7 @@ public class Consumer implements Runnable {
       }
     } catch (Exception e){
 //      System.out.println(e.getMessage());
-      res.addFailedOp(op);
+      res.addFailedOp(op.opNmae);
     }
   }
 
